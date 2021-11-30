f718394c0b16b71942bd9f65bc32505ca60a768e
Sagor31h2 <sagor31h2@gmail.com>
2021-11-30 21:15:36 +0600

    First test

---
+++ Random.Test/Fundamentals/ReservationTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Random.Fundamentals;
+
+namespace Random.Test.Fundamentals
+{
+    [TestFixture]
+    class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling__ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.That(result == true);
+        }
+    }
+}
